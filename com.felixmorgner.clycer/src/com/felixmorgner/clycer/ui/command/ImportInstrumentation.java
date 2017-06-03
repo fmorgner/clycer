6c4b8cba314b76f7193769bf51a699fa9b2f3d91
Felix Morgner <felix.morgner@gmail.com>
2017-06-03 12:19:42 +0200

    menu: prepare submenu

---
+++ com.felixmorgner.clycer/src/com/felixmorgner/clycer/ui/command/ImportInstrumentation.java
@@ -0,0 +1,14 @@
+package com.felixmorgner.clycer.ui.command;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class ImportInstrumentation extends AbstractHandler {
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException {
+        return null;
+    }
+
+}
